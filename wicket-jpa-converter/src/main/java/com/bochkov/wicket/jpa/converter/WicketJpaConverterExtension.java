37d3adf2d0005058ab42ccd6d8db7dfd2fb6bdde
bochkov-vi <bochkov2006@yandex.ru>
2021-02-15 13:04:52 +1200

    исправил проверку на composite id

--- wicket-jpa-converter/src/main/java/com/bochkov/wicket/jpa/converter/WicketJpaConverterExtension.java
+++ wicket-jpa-converter/src/main/java/com/bochkov/wicket/jpa/converter/WicketJpaConverterExtension.java
@@ -9,12 +9,14 @@ import org.apache.wicket.util.convert.IConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.JpaPersistableEntityInformation;
 import org.springframework.data.repository.core.EntityInformation;
 import org.springframework.data.repository.support.Repositories;
 
 @Slf4j
 @ApplicationInitExtension
 public class WicketJpaConverterExtension implements WicketApplicationInitConfiguration {
+
     @Autowired
     ApplicationContext context;
 
@@ -23,12 +25,14 @@ public class WicketJpaConverterExtension implements WicketApplicationInitConfigu
         repositories.forEach(clazz -> {
             log.info("Class = {}", clazz.getName());
             EntityInformation entityInformation = repositories.getEntityInformationFor(clazz);
+            boolean composite = entityInformation instanceof JpaPersistableEntityInformation && ((JpaPersistableEntityInformation) entityInformation).hasCompositeId();
+
             Class entityClass = entityInformation.getJavaType();
             Class idClass = entityInformation.getIdType();
             JpaRepository jpaRepository = (JpaRepository) repositories.getRepositoryFor(entityClass).get();
             log.info("idClass = {}", idClass.getName());
             IConverter idConverter = converterLocator.get(idClass);
-            if (idConverter == null) {
+            if (idConverter == null && composite) {
                 idConverter = new CompositeConverter(idClass);
                 converterLocator.set(idClass, idConverter);
             }
